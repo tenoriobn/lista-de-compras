5960284ad436e34156185ea4499bd32e49cd037e
Bruno Tenorio <tenoriobn@gmail.com>
2023-03-07 15:08:56 -0300

    Clonando e comparando objetos

---
+++ atividades/inserindoPropriedade.js
@@ -0,0 +1,25 @@
+cliente = {
+    nome: 'Mariana', 
+    idade: 35,
+    genero: 'Feminino'
+}
+
+/* Propriedade criada e adicionada no Objeto com o método de colchetes [] */
+    cliente['estado'] = 'Pernambuco';
+
+/* Cria um alerta para retornar a frase concatenada na tela */
+    alert('Olá, meu nome é ' + cliente.nome + ' e moro no estado de ' + cliente.estado)
+
+
+
+
+
+    
+/* Propriedade criado e adicionada no Objeto cliente com método do ponto . */
+    // cliente.estado = 'Pernambuco'
+
+/* Uma das formas de criar um alerta para retornar a frase concatenada na tela */
+    // cliente.mostrarItens = function() {alert('Olá, meu nome é ' + cliente.nome + ' e moro no estado de ' + cliente.estado)}
+
+/* Executa a função guardada na propriedade mostrarItens */
+    // cliente.mostrarItens();
