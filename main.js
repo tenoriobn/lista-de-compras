60a2d4a393475fdaca807bf59977cdc1d5bae083
Bruno Tenorio <tenoriobn@gmail.com>
2023-03-13 14:48:24 -0300

    Adicionando a funcionalidade de editar e substituir itens da lista

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = [];
+let itemAEditar;
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
@@ -54,6 +55,7 @@ function mostrarItem() {
                         <input type="text" class="is-size-5" value="${elemento.valor}"></input>
                     </div>
                     <div>
+                        <button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
                         <i class="fa-solid fa-trash is-clickable deletar"></i>
                     </div>
                 </li>
@@ -68,6 +70,31 @@ function mostrarItem() {
             valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value');
             listaDeItens[valorDoElemento].checar = evento.target.checked;
             mostrarItem();
-        })
-    })
+        });
+    });
+
+    const deletarObjeto = document.querySelectorAll('.deletar');
+
+    deletarObjeto.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value');
+            listaDeItens.splice(valorDoElemento, 1);
+            mostrarItem();
+        });
+    });
+
+    const editarItens = document.querySelectorAll('.editar');
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value');
+            mostrarItem();
+        });
+    });
+
+}
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`);
+    console.log(itemEditado.value)
 }
